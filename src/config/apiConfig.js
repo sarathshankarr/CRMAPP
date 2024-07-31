52035ca9ad20d9604f65dc3c7d026c895d74f679
sarathshankarr <sarath.s@codeverse.in>
2024-07-31 19:55:56 +0530

    logn screen 404

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -31,7 +31,6 @@ export const API = {
   ADD_NEW_CALL:'erpportal/api/master/addCall',
   GET_CALL_BY_ID:'erpportal/api/master/getCall',
   ADD_UPDATE_TASK:'erpportal/api/master/addTask',
-  ADD_NEW_CALL:'erpportal/api/master/addCall',
   GET_DISTRIBUTORS_DETAILS:'erpportal/api/distributors',
   GET_USER_IN_ACTIVE:'erpportal/api/users/updateUserInActive',
   GENERATE_CATE_LOG:'erpportal/api/ordermgmt/generateCatelog',
